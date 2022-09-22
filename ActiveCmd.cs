c59e2def77a663b97ba60d5ffc93aeeb93288d64
Mohamad Dbouk <Mohamad.Dbouk@treasuryxpress.com>
2022-09-22 16:49:55 +0300

    Add the project files

---
+++ ActiveCmd.cs
@@ -0,0 +1,18 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hal9000Cli
+{
+    [Command(Name = "active", Description = "Set slack status to Active")]
+    public class ActiveCmd
+    {
+        protected Task<int> OnExecute(CommandLineApplication app)
+        {
+            return Task.FromResult(0);
+        }
+    }
+}
